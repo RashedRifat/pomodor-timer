f80bfaa9d87a8adabbe715756d8df72f28adddc9
RashedRifat <59259308+RashedRifat@users.noreply.github.com>
2021-07-06 20:59:45 -0400

    add functonality

--- scripts.js
+++ scripts.js
@@ -7,6 +7,18 @@ let isClockRunning = false
 let workSessionDuration = 1500
 let currentTimeLeftInSession = 1500
 let breakSessionDuration = 300
+let timeSpentInCurrentSession = 0
+
+let type = 'Work'
+let isClockStopped = true
+let currrentTaskLabel = document.querySelector("#clock-task")
+
+let updatedWorkSessionDuration
+let updatedBreakSessionDuration
+let workDurationInput = document.querySelector('#input-work-duration')
+let breakDurationInput = document.querySelector('#input-break-duration')
+workDurationInput.value = '25'
+breakDurationInput.value = '5'
 
 
 // Add event listners for the project
@@ -22,6 +34,19 @@ stopButton.addEventListener("click", () => {
     toggleClock(True)
 })
 
+workDurationInput.addEventListener("input", () => {
+    updatedBreakSessionDuration = minuteToSeconds(workDurationInput.value)
+})
+
+breakDurationInput.addEventListener("input", () => {
+    updatedBreakSessionDuration = minuteToSeconds(breakDurationInput.value)
+})
+
+
+// Convert minutes to seconds 
+const minuteToSeconds = (mins) => {
+    return mins * 60
+}
 
 // Toggle the clock to either rest, start or stop 
 const toggleClock = (reset) => {
@@ -29,6 +54,13 @@ const toggleClock = (reset) => {
         stopClock()
 
     } else {
+
+        // Update the timers appropriately
+        if (isClockStopped) {
+            setUpdatedTimers()
+            isClockStopped = false
+        }
+
         if (isClockRunning === true) {
             // pause the timer
             clearInterval(clockTimer)
@@ -37,19 +69,62 @@ const toggleClock = (reset) => {
             // start the timer
             isClockRunning = true
             clockTimer = setInterval(() => {
-                currentTimeLeftInSession--
+                stepDown()
                 displayCurrentTimeLeftInSession()
             }, 1000)
+
+            isClockRunning = true
         }
     }
 };
 
+// Create a Step Down function 
+const stepDown = () => {
+
+    // If the current session still has time remaining
+    if (currentTimeLeftInSession > 0) {
+        currentTimeLeftInSession--
+        timeSpentInCurrentSession++
+
+    // If not, toggle the type of the timer
+    } else if (currentTimeLeftInSession === 0) {
+        timeSpentInCurrentSession = 0 
+
+        if (type === 'Work') {
+            currentTimeLeftInSession = breakSessionDuration
+            type = 'Break'
+            setUpdatedTimers()
+    
+            currentTaskLabel.value = 'Break'
+            currentTaskLabel.disabled = true
+            displaySessionLog('Work')
+
+        } else {
+            currentTimeLeftInSession = workSessionDuration
+            type = 'Work'
+            setUpdatedTimers()
+
+            if (currrentTaskLabel.value === "Break") {
+                currentTaskLabel.value = workSessionLabel
+            }
+            currentTaskLabel.disabled = false
+            displaySessionLog('Break')
+        }
+    }
+    displayCurrentTimeLeftInSession()
+}
+
 // Stop the clock 
 const stopClock = () => {
+    setUpdatedTimers()
+    displaySessionLog(type)
     clearInterval(clockTimer)
+    isClockStopped = true
     isClockRunning = false
     currentTimeLeftInSession = workSessionDuration
     displayCurrentTimeLeftInSession()
+    type = 'Work'
+    timeSpentInCurrentSession = 0
 };
 
 
@@ -71,3 +146,36 @@ const displayCurrentTimeLeftInSession = () => {
     result += `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`
     pomodoroTimer.innerText = result.toString()
 };
+
+
+// Display the log
+const displaySessionLog = (type) => {
+    const sessionsList = document.querySelector("#sessions")
+    const li = document.createElement("li")
+    
+    if (type === 'Work') {
+        sessionLabel = currentTaskLabel.value ? currentTaskLabel.value : 'Work'
+        workSessionLabel = sessionLabel
+      } else {
+        sessionLabel = 'Break'
+      }
+
+
+    let elapsedTime = parseInt(timeSpentInCurrentSession / 60)
+    elapsedTime = elapsedTime > 0 ? elapsedTime : "< 1"
+
+    const text = document.createTextNode(`${sessionLabel} : ${elapsedTime} min`)
+    li.appendChild(text)
+    sessionsList.appendChild(li)
+}
+
+// Update timers based on user input 
+const setUpdatedTimers = () => {
+    if (type === 'Work') {
+        currentTimeLeftInSession = updatedBreakSessionDuration ? updatedBreakSessionDuration : workSessionDuration
+        workSessionDuration = currentTimeLeftInSession
+    } else {
+        currentTimeLeftInSession = updatedBreakSessionDuration ? updatedBreakSessionDuration : breakSessionDuration
+        breakSessionDuration = currentTimeLeftInSession
+    }
+}
